47b7925d63bb51f42dff1a4cfb15ae284e5fd735
Docsilva <douglas.csilva@mercadolivre>
2022-10-27 12:49:02 -0300

    projeto calculadora

---
+++ Aulas java/primeira_aula/calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,29 @@
+package calculadora;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("###.##");
+		
+		int num1, num2, res;
+		
+		System.out.print("Digite o primeiro numero: ");
+		num1 = leia.nextInt();		
+		
+		System.out.print("Digite o segundo numero: ");
+		num2 = leia.nextInt();		
+		
+		System.out.println("\n" + num1 + " + " + num2 + " = " + (num1 + num2));
+		System.out.println("\n" + num1 + " - " + num2 + " = " + (num1 - num2));
+		System.out.println("\n" + num1 + " * " + num2 + " = " + (num1 * num2));
+		System.out.println("\n" + num1 + " / " + num2 + " = " + (num1 / num2));
+		System.out.println("\n" + num1 + " ˆ " + num2 + " = " + Math.pow(num1, num2));
+		System.out.println("\nA raiz de " + num1 + " = " + df.format(Math.sqrt (num1)));
+	}
+
+}
